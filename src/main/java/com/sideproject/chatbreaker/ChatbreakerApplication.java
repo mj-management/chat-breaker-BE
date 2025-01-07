e3307d911339088102929017088c234632373284
Gotprgmer <gofiction95@gmail.com>
2025-01-07 19:20:35 +0900

    git init

---
+++ src/main/java/com/sideproject/chatbreaker/ChatbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.chatbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/chatbreaker/ChatbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.chatbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
